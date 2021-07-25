c595d4b927e73b4297623cdc17ce5497fa8b945d
Aidan Mackey <57772122+A-Mackey@users.noreply.github.com>
2021-07-25 16:35:16 -0700

    Add comments and make paths absolute

--- src/main/java/panterix/CaptionGeneratorJavaAPI/GetCaption.java
+++ src/main/java/panterix/CaptionGeneratorJavaAPI/GetCaption.java
@@ -12,16 +12,18 @@ import java.util.Scanner;
 public class GetCaption {
 	private static String captionFile = "./src/main/java/panterix/CaptionGeneratorJavaApi/captions.txt";
 	private static String passwordFile = "./src/main/java/panterix/CaptionGeneratorJavaApi/password.txt";
+	
 	private static String cachedPassword;
 	private static ArrayList<String> captions = new ArrayList<String>();
 	
-	//Returns a random caption from the list of captions
+	// Returns a random caption from the list of captions
 	public static String get() {
 		double rand = Math.random() * captions.size();
 		int index = (int)rand;
 		return captions.get(index);
 	}
 	
+	// Adds a caption if the password is correct
 	public static boolean add(String caption, String password) {
 		if (checkPassword(password)) {
 			// If caption is not a duplicate, add it to the list
@@ -39,6 +41,7 @@ public class GetCaption {
 		return false;
 	}
 	
+	// Checks and makes sure the input is not a duplicate
 	private static boolean notADuplicate(String caption) {
 		for (String cap : captions) {
 			if (cap.compareTo(caption) == 0) {
@@ -54,7 +57,7 @@ public class GetCaption {
 		
 		// Updates the cache of captions by reading from file
 		try {
-		      File myObj = new File(captionFile);
+		      File myObj = new File(captionFile).getAbsoluteFile();
 		      Scanner myReader = new Scanner(myObj);
 		      while (myReader.hasNextLine()) {
 		        String data = myReader.nextLine();
@@ -69,9 +72,10 @@ public class GetCaption {
 		return captions.size();
 	}
 	
+	// Updates the password into the local cache
 	public static void updatePassword() {
 		try {
-			File pass = new File(passwordFile);
+			File pass = new File(passwordFile).getAbsoluteFile();
 			Scanner reader = new Scanner(pass);
 			
 			if (reader.hasNextLine()) {
@@ -82,6 +86,7 @@ public class GetCaption {
 		} catch (Exception e) {}
 	}
 	
+	// Checks the make sure the password is correct
 	private static boolean checkPassword(String password) {
 		return (password.compareTo(cachedPassword) == 0);
 	}
