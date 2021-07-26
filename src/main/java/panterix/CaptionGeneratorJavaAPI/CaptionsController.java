452c1e6d6a3b61711f3e8ca4aaa90ff6b8df620e
Aidan Mackey <57772122+A-Mackey@users.noreply.github.com>
2021-07-25 17:31:38 -0700

    Allow all cors

--- src/main/java/panterix/CaptionGeneratorJavaAPI/CaptionsController.java
+++ src/main/java/panterix/CaptionGeneratorJavaAPI/CaptionsController.java
@@ -1,10 +1,12 @@
 package panterix.CaptionGeneratorJavaAPI;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class CaptionsController {
 	
