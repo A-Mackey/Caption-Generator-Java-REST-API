86eee6f037494be0ac3b53328185eedf24a20954
Aidan Mackey <57772122+A-Mackey@users.noreply.github.com>
2021-07-25 17:02:40 -0700

    Change to local storage in prep for database integration

--- src/main/java/panterix/CaptionGeneratorJavaAPI/GetCaption.java
+++ src/main/java/panterix/CaptionGeneratorJavaAPI/GetCaption.java
@@ -1,18 +1,7 @@
 package panterix.CaptionGeneratorJavaAPI;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class GetCaption {
-	private static String captionFile = "./src/main/java/panterix/CaptionGeneratorJavaApi/captions.txt";
-	private static String passwordFile = "./src/main/java/panterix/CaptionGeneratorJavaApi/password.txt";
-	
 	private static String cachedPassword;
 	private static ArrayList<String> captions = new ArrayList<String>();
 	
@@ -29,10 +18,6 @@ public class GetCaption {
 			// If caption is not a duplicate, add it to the list
 			if (notADuplicate(caption)) {
 				captions.add(caption);
-				caption += '\n';
-				try {
-		            Files.write(Paths.get(captionFile), caption.getBytes(), StandardOpenOption.APPEND);
-		        } catch (IOException e) { return false; }
 				return true;
 			}
 			return false;
@@ -55,35 +40,12 @@ public class GetCaption {
 	public static int update() {
 		captions.clear();
 		
-		// Updates the cache of captions by reading from file
-		try {
-		      File myObj = new File(captionFile).getAbsoluteFile();
-		      Scanner myReader = new Scanner(myObj);
-		      while (myReader.hasNextLine()) {
-		        String data = myReader.nextLine();
-		        captions.add(data);
-		      }
-		      myReader.close();
-		    } catch (FileNotFoundException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-		
 		return captions.size();
 	}
 	
 	// Updates the password into the local cache
 	public static void updatePassword() {
-		try {
-			File pass = new File(passwordFile).getAbsoluteFile();
-			Scanner reader = new Scanner(pass);
-			
-			if (reader.hasNextLine()) {
-				cachedPassword = reader.nextLine();
-			}
-			
-			reader.close();
-		} catch (Exception e) {}
+		cachedPassword = PasswordGetter.password;
 	}
 	
 	// Checks the make sure the password is correct
